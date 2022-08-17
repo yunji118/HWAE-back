c5daa492dd2c5e5c20bb662845b18ed6ba1137da
Yunji Ha <67634926+yunji118@users.noreply.github.com>
2022-08-17 13:45:38 +0900

    Delete QnAboard directory

--- QnAboard/src/main/java/com/HWAEweb/QnAboard/repository/QnacommentRepository.java
+++
@@ -1,9 +0,0 @@
-package com.HWAEweb.QnAboard.repository;
-
-import com.HWAEweb.QnAboard.entity.Qnacomment;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface QnacommentRepository extends JpaRepository<Qnacomment, Integer> {
-}
