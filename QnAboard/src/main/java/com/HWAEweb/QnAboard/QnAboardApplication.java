d7372c389dedb69cfc70afec9115999587b8ece3
Yunji Ha <67634926+yunji118@users.noreply.github.com>
2022-08-17 13:47:31 +0900

    Add files via upload

---
+++ QnAboard/src/main/java/com/HWAEweb/QnAboard/QnAboardApplication.java
@@ -0,0 +1,13 @@
+package com.HWAEweb.QnAboard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QnAboardApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QnAboardApplication.class, args);
+	}
+
+}
---
+++ QnAboard/src/main/java/com/HWAEweb/QnAboard/service/QnaboardService.java
@@ -0,0 +1,45 @@
+package com.HWAEweb.QnAboard.service;
+
+import com.HWAEweb.QnAboard.entity.Qnaboard;
+import com.HWAEweb.QnAboard.entity.Qnacomment;
+import com.HWAEweb.QnAboard.repository.QnaboardRepository;
+import com.HWAEweb.QnAboard.repository.QnacommentRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.stereotype.Service;
+
+@Service
+public class QnaboardService {
+
+    @Autowired
+    private QnaboardRepository qnaboardRepository;
+
+
+
+    //QnA 글 작성 처리
+    public void qnawrite(Qnaboard qnaboard){
+
+        qnaboardRepository.save(qnaboard);
+    }
+
+    //QnA 게시글 리스트 처리
+    public Page<Qnaboard> qnaboardList(Pageable pageable){
+
+        return qnaboardRepository.findAll(pageable);
+    }
+
+    //QnA 특정 게시글 불러오기
+    public Qnaboard qnaboardView(Integer postID){
+
+        return qnaboardRepository.findById(postID).get();
+    }
+
+    //QnA 특정 게시글 삭제
+    public void qnaDelete(Integer postID){
+
+        qnaboardRepository.deleteById(postID);
+    }
+
+
+}
